4ced4c026c3aad514828c9d0279f6328579c386f
gyssa-prince <91846911+gyssa-prince@users.noreply.github.com>
2022-03-24 08:42:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import router from './routes/index.js'
 import swaggerDocs from './swagger.js'
 
 const app = express()
-const port = 4000
+const port = 1000
 
 app.use(express.json())
 app.use(router)
